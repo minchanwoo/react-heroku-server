b9d1db76efd6e60e272cde3ef59f59f2d3bc6363
minchanwoo <dearmcw0070@gmail.com>
2020-08-11 07:12:45 +0900

    커리수정

--- dist/app.js
+++ dist/app.js
@@ -21,31 +21,22 @@ app.use(_express["default"].json());
 app.use(_express["default"].urlencoded({
   extended: true
 }));
-app.get('/', function (req, res) {
-  res.send('Hello Node');
-});
-app.post('/join', /*#__PURE__*/function () {
+app.get('/', /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(req, res) {
-    var _req$body, nick, name, result;
-
+    var result;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _req$body = req.body, nick = _req$body.nick, name = _req$body.name;
-            _context.next = 3;
-            return _models["default"].User.create({
-              nick: nick,
-              name: name
+            _context.next = 2;
+            return _models["default"].User.findAll({
+              order: [['ID', 'DESC']]
             });
+
+          case 2:
+            result = _context.sent;
 
           case 3:
-            result = _context.sent;
-            res.send({
-              result: result
-            });
-
-          case 5:
           case "end":
             return _context.stop();
         }
@@ -55,6 +46,39 @@ app.post('/join', /*#__PURE__*/function () {
 
   return function (_x, _x2) {
     return _ref.apply(this, arguments);
+  };
+}());
+app.post('/join', /*#__PURE__*/function () {
+  var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(req, res) {
+    var _req$body, nick, name, result;
+
+    return regeneratorRuntime.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _req$body = req.body, nick = _req$body.nick, name = _req$body.name;
+            _context2.next = 3;
+            return _models["default"].User.create({
+              nick: nick,
+              name: name
+            });
+
+          case 3:
+            result = _context2.sent;
+            res.send({
+              result: result
+            });
+
+          case 5:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2);
+  }));
+
+  return function (_x3, _x4) {
+    return _ref2.apply(this, arguments);
   };
 }());
 app.listen(PORT, function () {
